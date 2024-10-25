f01ca13289bc419da36a959bf991c0ceee024ba6
Obin <pradibtabintang123@gmail.com>
2024-10-25 13:39:27 +0700

    Add Selection Sort

--- portofolio.py
+++ portofolio.py
@@ -14,3 +14,15 @@ class SortingAlgorithms:
                 if arr[j] > arr[j+1]:
                     arr[j], arr[j+1] = arr[j+1], arr[j]
         return arr
+
+    @staticmethod
+    def selection_sort(arr: List[int]) -> List[int]:
+        n = len(arr)
+        arr = arr.copy()
+        for i in range(n):
+            min_idx = i
+            for j in range(i+1, n):
+                if arr[j] < arr[min_idx]:
+                    min_idx = j
+            arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        return arr
