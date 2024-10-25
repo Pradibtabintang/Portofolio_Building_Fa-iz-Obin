fce9448bf7169c4a0be45e7bc05a8c952a0c8033
Faiz <ismail1.faiz1@gmail.com>
2024-10-25 13:58:37 +0700

    Add another 3 sorted function

--- portofolio.py
+++ portofolio.py
@@ -38,3 +38,44 @@ class SortingAlgorithms:
                 j -= 1
             arr[j+1] = key
         return arr
+    @staticmethod
+    def quick_sort(arr: List[int]) -> List[int]:
+        arr = arr.copy()
+        if len(arr) <= 1:
+            return arr
+        else:
+            pivot = arr[0]
+            less = [x for x in arr[1:] if x <= pivot]
+            greater = [x for x in arr[1:] if x > pivot]
+            return SortingAlgorithms.quick_sort(less) + [pivot] + SortingAlgorithms.quick_sort(greater)
+    @staticmethod
+    def merge_sort(arr: List[int]) -> List[int]:
+        arr = arr.copy()
+        if len(arr) <= 1:
+            return arr
+        
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+        
+        left = SortingAlgorithms.merge_sort(left)
+        right = SortingAlgorithms.merge_sort(right)
+        
+        return SortingAlgorithms.merge(left, right)
+    
+    @staticmethod
+    def merge(left: List[int], right: List[int]) -> List[int]:
+        result = []
+        i = j = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+        
+        result.extend(left[i:])
+        result.extend(right[j:])
+        return result
