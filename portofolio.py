36081f3ff8e70ea103eabb7e7420a0e9b3a11f14
Faiz <ismail1.faiz1@gmail.com>
2024-10-25 13:41:50 +0700

    Add insertion sort

--- portofolio.py
+++ portofolio.py
@@ -26,3 +26,15 @@ class SortingAlgorithms:
                     min_idx = j
             arr[i], arr[min_idx] = arr[min_idx], arr[i]
         return arr
+    
+    @staticmethod
+    def insertion_sort(arr: List[int]) -> List[int]:
+        arr = arr.copy()
+        for i in range(1, len(arr)):
+            key = arr[i]
+            j = i-1
+            while j >= 0 and key < arr[j]:
+                arr[j+1] = arr[j]
+                j -= 1
+            arr[j+1] = key
+        return arr
