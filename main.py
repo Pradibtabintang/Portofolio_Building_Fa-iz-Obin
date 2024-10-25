43ca662eff510c6e06eebaa487420d66e54311b9
Faiz <ismail1.faiz1@gmail.com>
2024-10-25 13:20:12 +0700

    Add bubble sort

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+import time
+import matplotlib.pyplot as plt
+from typing import List
+
+
+class SortingAlgorithms:
+    @staticmethod
+    def bubble_sort(arr: List[int]) -> List[int]:
+        n = len(arr)
+        arr = arr.copy()
+        for i in range(n):
+            for j in range(0, n-i-1):
+                if arr[j] > arr[j+1]:
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+        return arr
+
+
+
+
